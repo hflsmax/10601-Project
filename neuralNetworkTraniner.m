8961d83dcebe3a33a934fc3d2babd071b48e0b90
Max Ma <cma1@cyh100-a4.cyert.andrew.cmu.edu>
2015-12-02 16:15:18 -0500

    Working for ideal data

--- neuralNetworkTraniner.m
+++ neuralNetworkTraniner.m
@@ -2,30 +2,36 @@ function [ hiddenW, outputW ] = neuralNetworkTraniner( x, y )
 %neuralNetworkTrainer is a simple nn trainer that use backpropogation,
 %using 0 as starting point
 
-% rng(1);
+% rng(243);
 [n, p] = size(x);
-hiddenn = 50;
-outputn = 10;
+hiddenn = 4;
+outputn = max(y) + 1;
 hiddenW = rand(p, hiddenn);
-for i = 1:hiddenn
-    hiddenW(:, i) = hiddenW(:, i) / sum(hiddenW(:, i));
-end
+% sumw = sum(sum(hiddenW));
+% for i = 1:hiddenn
+%     hiddenW(:, i) = hiddenW(:, i) / sumw;
+% end
 % repmat(0, p, hiddenn);
 outputW = rand(hiddenn, outputn);
-for i = 1:outputn
-    outputW(:,i) = outputW(:,i) / sum(outputW(:,i));
-end
+% sumw = sum(sum(outputW));
+% for i = 1:outputn
+%     outputW(:,i) = outputW(:,i) / sumw;
+% end
 % repmat(0, hiddenn, outputn);
 % lambda = 0.0008;
-lambda = .00001;
+lambda = .1;
 
-
+for k = 1:50
 for i = 1:n
-    output = zeros(outputn, 1);
+    output = -ones(outputn, 1);
     output(y(i)+1) = 1;
     [hiddenW, outputW] = backPropogation(p, hiddenn, outputn,...
                                          x(i,:), output, ...
                                          hiddenW, outputW, lambda);
+                                    
+end
+hiddenW
+
 end
 
 
